26394fe54ba1b77c94a7cdc44761b5be9cd27c7a
Tony Hoyin Liu <tonyhoyinliu@gmail.com>
2016-08-27 18:13:37 -0700

    added action

---
+++ frontend/actions.js
@@ -0,0 +1,10 @@
+const selectCurrency = function(baseCurrency, rates) {
+  return({
+    type: "SWITCH_CURRENCY",
+    baseCurrency: baseCurrency,
+    rates: rates
+  })
+};
+
+// window.selectCurrency = selectCurrency;
+export default selectCurrency;
